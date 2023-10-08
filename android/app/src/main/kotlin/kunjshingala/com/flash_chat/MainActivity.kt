6ea283c714d6358d51049bc455135a2670fea244
Kunj Shingala <kunjshingala.p@gmail.com>
2023-10-08 09:19:16 +0530

    First Commit

---
+++ android/app/src/main/kotlin/kunjshingala/com/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package kunjshingala.com.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      //can't use both at same time home property and initialRoute .
+      // home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
